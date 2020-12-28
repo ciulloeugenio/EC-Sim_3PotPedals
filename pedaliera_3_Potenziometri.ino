56b688334a4f1768e5c54a14bfdabea32a26095d
ciulloeugenio <65494496+ciulloeugenio@users.noreply.github.com>
2020-12-28 17:59:58 +0100

    Update pedaliera_3_Potenziometri.ino

--- pedaliera_3_Potenziometri.ino
+++ pedaliera_3_Potenziometri.ino
@@ -1,15 +1,59 @@
+/*
+┏━━━┓╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┏━━━┓╋╋╋┏┓┏┓
+┃┏━━┛╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┃┏━┓┃╋╋╋┃┃┃┃
+┃┗━━┳┓┏┳━━┳━━┳━┓┏┳━━┓┃┃╋┗╋┳┓┏┫┃┃┃┏━━┓
+┃┏━━┫┃┃┃┏┓┃┃━┫┏┓╋┫┏┓┃┃┃╋┏╋┫┃┃┃┃┃┃┃┏┓┃
+┃┗━━┫┗┛┃┗┛┃┃━┫┃┃┃┃┗┛┃┃┗━┛┃┃┗┛┃┗┫┗┫┗┛┃
+┗━━━┻━━┻━┓┣━━┻┛┗┻┻━━┛┗━━━┻┻━━┻━┻━┻━━┛
+╋╋╋╋╋╋╋┏━┛┃
+╋╋╋╋╋╋╋┗━━┛
+
+
+███████╗░█████╗░░░░░░░░██████╗██╗███╗░░░███╗
+██╔════╝██╔══██╗░░░░░░██╔════╝██║████╗░████║
+█████╗░░██║░░╚═╝█████╗╚█████╗░██║██╔████╔██║
+██╔══╝░░██║░░██╗╚════╝░╚═══██╗██║██║╚██╔╝██║
+███████╗╚█████╔╝░░░░░░██████╔╝██║██║░╚═╝░██║
+╚══════╝░╚════╝░░░░░░░╚═════╝░╚═╝╚═╝░░░░░╚═╝
+
+██████╗░██████╗░░█████╗░████████╗██████╗░███████╗██████╗░░█████╗░██╗░░░░░░██████╗
+╚════██╗██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██║░░░░░██╔════╝
+░█████╔╝██████╔╝██║░░██║░░░██║░░░██████╔╝█████╗░░██║░░██║███████║██║░░░░░╚█████╗░
+░╚═══██╗██╔═══╝░██║░░██║░░░██║░░░██╔═══╝░██╔══╝░░██║░░██║██╔══██║██║░░░░░░╚═══██╗
+██████╔╝██║░░░░░╚█████╔╝░░░██║░░░██║░░░░░███████╗██████╔╝██║░░██║███████╗██████╔╝
+╚═════╝░╚═╝░░░░░░╚════╝░░░░╚═╝░░░╚═╝░░░░░╚══════╝╚═════╝░╚═╝░░╚═╝╚══════╝╚═════╝░
+
+
+
+Copyright (c) 2020 Eugenio Ciullo
+
+
+Gli sketch in questo repository sono stati creati da Eugenio Ciullo.
+Verrà creato un Controller HID disposto di 3 Assi funzionanti attraverso 3 potenziometri collegati in A0 A1 e A3.
+
+Bisogna installare nella Documents/Arduino directory la libreria ArduinoJoystickLibrary creata da MHeironimus reperibile al seguente link:
+https://github.com/MHeironimus/ArduinoJoystickLibrary
+
+Buon Divertimento
+*/
+
+
 
 #include <Joystick.h>
 
 int Acceleratore = 0;
 int Freno = 0;
 int Frizione = 0;
+
+
+// Se volete calibrare il range degli assi, sostituire i seguenti valori
 const int ZonainferioreAcceleratore = 0;
 const int ZonasuperioreAcceleratore = 1023;
 const int ZonainferioreFreno = 0;
 const int ZonasuperioreFreno = 1023;
 const int ZonainferioreFrizione = 0;
 const int ZonasuperioreFrizione = 1023;
+
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
   0, 0,                  // Button Count, Hat Switch Count
@@ -23,7 +67,10 @@ void setup() {
   Joystick.setRxAxisRange(ZonainferioreAcceleratore, ZonasuperioreAcceleratore);
   Joystick.setRyAxisRange(ZonainferioreFreno, ZonasuperioreFreno);
   Joystick.setRzAxisRange(ZonainferioreFrizione, ZonasuperioreFrizione);
- // Serial.begin(9600);
+  
+//decommentare il prossimo valore se si vuole aprire la seriale
+
+  // Serial.begin(9600);
     }
 
 void loop() {
@@ -33,11 +80,15 @@ void loop() {
   Joystick.setRxAxis(Acceleratore);
   Joystick.setRyAxis(Freno);
   Joystick.setRzAxis(Frizione);
- // Serial.print("Acceleratore : \t");
-//  Serial.print(Acceleratore);
-//  Serial.print("\t \t Freno: \t");
-//  Serial.print(Freno);
-//  Serial.print("\t Frizione : \t");
-//  Serial.println(Frizione);  
+  
+// Decommentare le seriali eliminando /* e */ alla fine dei Serial.print
+/* 
+    Serial.print("Acceleratore : \t");
+    Serial.print(Acceleratore);
+    Serial.print("\t \t Freno: \t");
+    Serial.print(Freno);
+    Serial.print("\t Frizione : \t");
+    Serial.println(Frizione);  
+*/
   delay(5);
 }
